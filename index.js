11a5a601127922b2b382587d62ede892055d17ce
Ubuntu <ubuntu@ip-172-31-44-181.us-west-2.compute.internal>
2015-07-12 23:39:17 -0600

    firsties

---
+++ index.js
@@ -0,0 +1,18 @@
+/* built in file handling plugin */
+define(function(){
+	return {
+		init : function(config, callback){
+			//console.log('file setup', config);
+			callback();
+		},
+		encode : function(inp, callback){
+			
+		},
+		decode : function(inp, callback){
+			
+		},
+		remove : function(id, callback){
+		
+		}
+	}
+});
