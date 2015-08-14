ffa4f035a44b624e8999104bfd4dd96902c0d452
Ubuntu <ubuntu@ip-172-31-44-181.us-west-2.compute.internal>
2015-08-14 17:38:34 -0600

    overhaul to new thumos system

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
 /* built in file handling plugin */
 define(['underscore', 'async'], function(_, async){
 	return {
-		name : 'file',
-		init : function(thumos, prequire, nodeRequire, router, config, setConfig, propName, callback){
-			var crypto = prequire('crypto');
-			
-			var aws = nodeRequire('aws-sdk');
-			aws.config.region = 'us-west-2';
-			var skipper = nodeRequire('skipper')();			
-			
-			router.route('/')
-			.post(function(req, res, next){
-				skipper(req, res, function(e){ //call skipper
-					if(e) res.json({error : 'file'});
-					else{
-						var files = req.file('files').on('error', function(){}); 
-						var file = files._files[0]; //take first file ignore the rest
-						if(!file) res.json({error : 'no file'});
-						else{
-							/* gen random id for use in s3 */
-							var id = crypto.randomBytes(16).toString('hex');
-							var s3 = new aws.S3({params: {Bucket: 'transcendencelaw', Key : setConfig.name+'/'+propName+'/'+id}});
-							s3.upload({Body : file.stream}).send(function(e, data){
-								if(e) res.json({error : e.toString()})
-								else res.json({
-									id : id,
-									name : file.stream.filename,
-									size : file.stream.byteCount
-								});
-							});	
-						}
-					}
-				});	
-			})
-			.delete(function(req, res, next){
-				
-			});
-			
-			callback();
+		encode : function(value, callback){
+		
 		},
-		encode : function(inp, callback){
-			var ajax = require('ajax');
-		},
-		decode : function(inp, callback){
-			
-		},
-		remove : function(id, callback){
+		decode : function(value, callback){
 		
 		}
 	}
---
+++ server.js
@@ -0,0 +1,70 @@
+var crypto = require('crypto');
+var aws = require('aws-sdk');
+aws.config.region = 'us-west-2';
+var fs = require('fs');
+var path = require('path');
+var skipper = require('skipper')();
+var express = require('express');	
+
+module.exports = function(config){
+	var api = {
+		init : function(gconfig, callback){
+			var router = express.Router();
+			router.route('/').post(function(req, res, next){
+				skipper(req, res, function(e){ //call skipper
+					if(e) res.json({error : 'file'});
+					else{
+						var files = req.file('files').on('error', function(){}); 
+						var file = files._files[0]; //take first file ignore the rest
+						if(!file) res.json({error : 'no file'});
+						else{
+							/* gen random id for use in s3 */
+							var id = crypto.randomBytes(16).toString('hex');
+							var tmppath = path.join(config.tmpdir, id);
+							var ws = fs.createWriteStream(tmppath);
+							file.stream.pipe(ws).on('finish', function(e){
+								if(e) res.json({error : 'something'});
+								else{
+									/* clean up after a min */
+									setTimeout(function(){
+										fs.unlink(tmppath, function(){});	
+									}, (config.timeout||60)*1000);
+									
+									/* respond to request */
+									res.json({
+										id : id,
+										temporary : true,
+										name : file.stream.filename,
+										size : file.stream.byteCount
+									});
+								}
+							});
+						}
+					}
+				});
+			});
+			gconfig.app.use('/bin', router);
+			callback();
+		},
+		finalize : function(value, callback){
+			/* move temporary file to s3 and clean up */
+			var tmppath = path.join(config.tmpdir, value.id);
+			var rs = fs.createReadStream(tmppath).on('error', function(e){
+				callback('not found');
+			});
+			var s3 = new aws.S3({params: {Bucket: config.bucket, Key : value.id}});
+			s3.upload({Body : rs}).send(function(e, data){
+				if(e) res.json({error : e.toString()})
+				else fs.unlink(tmppath, function(e){
+					delete value.temporary;
+					callback(e, value);
+				});
+			});
+		},
+		purge : function(value, callback){
+			var s3 = new aws.S3();
+			s3.deleteObject({Bucket: config.bucket, Key : value.id}, callback);
+		}
+	}
+	return api;
+}
